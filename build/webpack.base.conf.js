ce1b9d102141e17b23150d4e82ddbdc543d712bf
flyerJay <876173839@qq.com>
2016-12-18 23:04:24 +0800

    Update webpack.base.conf.js

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -27,7 +27,7 @@ module.exports = {
       'src': path.resolve(__dirname, '../src'),
       'assets': path.resolve(__dirname, '../src/assets'),
       'components': path.resolve(__dirname, '../src/components'),
-      'jquery': 'jquery'
+      '$': require("jquery")
     }
   },
   resolveLoader: {
